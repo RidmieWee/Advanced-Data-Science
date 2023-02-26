7da01fcd95cc8025715eb3f02fbcb5bf4c181aaa
RidmieWee <ridmieweerakotuwa1999@gmail.com>
2023-02-26 20:29:42 +0000

    did some changes and finished the first part

--- Assignment_1_Visualization_Solution.py
+++ Assignment_1_Visualization_Solution.py
@@ -10,55 +10,53 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load country data into dataframe
-df_countries = pd.read_csv("C:/Users/Ridmi Weerakotuwa/OneDrive - University of Hertfordshire/UH/ADS/Assignment 1/Advanced-Data-Science/Countries_gdp.csv", sep=";")
-
-# load organization data into dataframe
-df_organization = pd.read_csv("C:/Users/Ridmi Weerakotuwa/OneDrive - University of Hertfordshire/UH/ADS/Assignment 1/Advanced-Data-Science/Organization_gdp.csv")
-
-# load population data into dataframe
-df_population = pd.read_csv("C:/Users/Ridmi Weerakotuwa/OneDrive - University of Hertfordshire/UH/ADS/Assignment 1/Advanced-Data-Science/Population.csv")
+df_countries = pd.read_csv("Countries_gdp.csv", sep=";")
 
 # explore the first rows
 print(df_countries.head())
-print(df_organization.head())
-print(df_population.head())
 
 # explore the information about data
 print(df_countries.info())
-print(df_organization.info())
-print(df_population.info())
 
-# transform the populations table seperate years columns into one year column
-df_population = pd.melt(df_population,
-                        id_vars = ["Country Name",
-                                   "Country Code",
-                                   "Indicator Name",
-                                   "Indicator Code"],
-                        value_vars = df_population.iloc[:, 4:-1].columns,
-                        var_name = "Year",
-                        value_name = ("total"))
+# create dataframes for plot line graph
+df_china = df_countries[df_countries["country_name"]=="China"]
+df_usa = df_countries[df_countries["country_name"]=="United States of America"]
+df_japan = df_countries[df_countries["country_name"]=="Japan"]
+df_germany = df_countries[df_countries["country_name"]=="Germany"]
+df_uk = df_countries[df_countries["country_name"]=="United Kingdom of Great Britain and Northern Ireland"]
 
-# explore the new dataframe
-print(df_population.head())
-
-# rename the total column into population
-df_population = df_population.rename(columns = {"total": "Population"})
-print(df_population.info())
-
-
-df_china = df_population[df_population["Country Name"]=="China"]
-df_india = df_population[df_population["Country Name"]=="India"]
-
+# explore the new dataframes
 print(df_china)
+print(df_usa)
+print(df_japan)
+print(df_germany)
+print(df_uk)
 
-plt.figure()
-plt.plot(df_china["Year"], df_china["Population"], label = "China")
-plt.plot(df_india["Year"], df_india["Population"], label = "India")
-plt.xscale(value="linear")
+# create function for plot line chart
+def make_line_chart():
+    """ plot the line graph using above created data frames """
 
-plt.xlabel("Year")
-plt.ylabel("GDP (2015 $)")
-plt.legend()
-plt.show()
+    plt.figure()
+
+    plt.plot(df_china["year"], df_china["total_gdp"], label = "China")
+    plt.plot(df_usa["year"], df_usa["total_gdp"], label = "USA")
+    plt.plot(df_japan["year"], df_japan["total_gdp"], label = "Japan")
+    plt.plot(df_germany["year"], df_germany["total_gdp"], label = "Germany")
+    plt.plot(df_uk["year"], df_uk["total_gdp"], label = "UK")
+
+    plt.xlabel("Year")
+    plt.ylabel("Total GDP")
+    plt.title("Total GDP disribution per year ")
+    plt.legend()
+
+    plt.savefig("line_chart.png")
+    plt.show()
+
+# call the make_line_chart function
+make_line_chart()
+
+print(df_countries["income_group"].unique())
+
+
 
 
