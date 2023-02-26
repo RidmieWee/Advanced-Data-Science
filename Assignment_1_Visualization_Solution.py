40c6dc7d7599df26410f61f7c35020011e54f084
RidmieWee <ridmieweerakotuwa1999@gmail.com>
2023-02-26 11:31:49 +0000

    data files are loaded

--- Assignment_1_Visualization_Solution.py
+++ Assignment_1_Visualization_Solution.py
@@ -5,5 +5,22 @@ Created on Sat Feb 25 11:21:11 2023
 @author: Ridmi Weerakotuwa
 """
 
-print("Hello World")
-print("Rid")
+# import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load country data into dataframe
+df_countries = pd.read_csv("C:/Users/Ridmi Weerakotuwa/OneDrive - University of Hertfordshire/UH/ADS/Assignment 1/Advanced-Data-Science/countries_gdp_hist.csv", sep=";")
+
+# load country data into dataframe
+df_organization = pd.read_csv("C:/Users/Ridmi Weerakotuwa/OneDrive - University of Hertfordshire/UH/ADS/Assignment 1/Advanced-Data-Science/organizations_gdp_hist.csv")
+
+# get the first rows
+print(df_countries.head())
+print(df_organization.head())
+
+# get the information about data
+print(df_countries.info())
+print(df_organization.info())
+
+
